f502234ada111f18375a3a68ea1a713e1202a985
Marcelo Alvarez <rmalvarezq@utn.edu.ec>
2021-02-17 15:42:39 -0500

    implementación del mapa terminado
    uso del apikey y mapa

--- lib/pages/mapa_page.dart
+++ lib/pages/mapa_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mapas_gps_flutter/bloc/mi_ubicacion/mi_ubicacion_bloc.dart';
 
 class MapaPage extends StatefulWidget {
@@ -32,18 +33,26 @@ class _MapaPageState extends State<MapaPage> {
       ),
       body: BlocBuilder<MiUbicacionBloc, MiUbicacionState>(
           builder: (_, state) => crearMapa(state)),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.arrow_back),
-        elevation: 10,
-        onPressed: () {
-          Navigator.pushReplacementNamed(context, 'acceso_gps');
-        },
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   child: Icon(Icons.arrow_back),
+      //   elevation: 10,
+      //   onPressed: () {
+      //     Navigator.pushReplacementNamed(context, 'acceso_gps');
+      //   },
+      // ),
     );
   }
 
   Widget crearMapa(MiUbicacionState state) {
     if (!state.existeUbicacion) return Center(child: Text('Ubicando...'));
-    return Text('${state.ubicacion.latitude},${state.ubicacion.longitude}');
+    final cameraPosition =
+        new CameraPosition(target: state.ubicacion, zoom: 15);
+    return GoogleMap(
+      initialCameraPosition: cameraPosition,
+      mapType: MapType.normal,
+      myLocationEnabled: true,
+      myLocationButtonEnabled: false,
+      zoomControlsEnabled: false,
+    );
   }
 }
